5e1ad18aaf0d60af8e628e06ecafce70e73b54fc
junya3 <obarajunya.0728@gmail.com>
2025-01-18 11:36:39 +0900

    データのインポート

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+ini_set('display_errors', '1');
+
+$host = 'localhost';
+$dbname = 'it_recipe_practice';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
